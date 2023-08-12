1ad873470d403086342092029d2f7f6f97f44bfb
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-08-12 23:48:44 +0000

    Adds create actor form

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,22 @@
 class ActorsController < ApplicationController
+  def create
+    a = Actor.new
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+
+    a.save
+
+    redirect_to("/actors")
+    # Retrieve the user's inputs from params
+    # Create a record in the movie table
+    # Populate each column with the user input
+    # Save
+    #Redirect the user back to the /movies URL
+  end
+
+  
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
