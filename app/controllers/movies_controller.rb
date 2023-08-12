dbfab5c2e797ff73c85ea5de1f80d4b4a4021b40
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-08-12 00:01:51 +0000

    Adds an edit form for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,27 @@
 class MoviesController < ApplicationController
+  def update
+    # Get the ID out of params
+    m_id = params.fetch("the_id")
+
+    # Loop up the exsiting record
+    matching_records = Movie.where({ id: m_id })
+    the_movie = matching_records.at(0)
+
+    # Overwrite each column with the values from user inputs
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    # Save
+    the_movie.save
+
+    # Redirect to the movie details page
+    redirect_to("/movies/#{the_movie.id}")
+  end
+  
   def create
     m = Movie.new
     m.title = params.fetch("the_title")
@@ -15,7 +38,6 @@ class MoviesController < ApplicationController
     # Create a record in the movie table
     # Populate each column with the user input
     # Save
-
     #Redirect the user back to the /movies URL
   end
 
